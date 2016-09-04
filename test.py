54cae9e1c5bf187a844c71e75ced391fa4c14db2
MuradTcen <oops-1.3@yandex.ru>
2016-09-04 21:39:29 +0500

    Third Commit

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #!bin/python3
 import xlrd
+import pylab
+from matplotlib import mlab
+import numpy as np
 
 
 '''
@@ -7,34 +10,36 @@ print
 Универсум, Носитель, Точки перехода, унимодальность функции принадлежности,
 нормально или субнормальное нечеткого множества, высота, ядро, границы
 нечеткого множества
-
+links:
+http://www.bogotobogo.com/python/python_numpy_array_tutorial_basic_A.php
+http://www.programcreek.com/python/example/504/numpy.array
+http://cs231n.github.io/python-numpy-tutorial/#matplotlib
+koldunov.net
 '''
-def plot():
-    print (' ')
+   
+def ploting(l0):
+    #formatedYVector=['%.2f' % elem for elem in ylist ]
+    x=np.arange(l0[0],l0[1],l0[2])
+    y=l0[3]*x*x-l0[4]*x+l0[5]
+    pylab.plot(x,y)
+    pylab.show()
 
-def subnormalToNormal():
-    print (' ')
-def printPropertyFuzzySet(l0,l1,l2,l3,l4):
-    print ('Универсум:')
-    for i in range(len(l1)):
-        print (l1[i],' ',end='')
+def subnormalToNormal(l1,l2,l3,l4):
     print ('')
     for i in range(len(l1)):
-        print ('--- ',end=' ')
-    print ('')
-    for i in range(len(l1)):
-        print (l2[i],' ',end='')
-    print ('')
+        l1[i]/=max(l1)
     for i in range(len(l3)):
-        print (l3[i],' ',end='')
-    print ('')
-    for i in range(len(l4)):
-        print ('--- ',end=' ')
-    print ('')
-    for i in range(len(l4)):
-        print (l4[i],' ',end='')
+        l3[i]/=max(l3)
+    print ('  '.join(map(str, l1))+'\n'+'  '.join(map(str, l2)),'\n')
+    print ('  '.join(map(str, l3))+'\n'+'  '.join(map(str, l4)),'\n')
+    
+def printPropertyFuzzySet(l1,l2,l3,l4):
+    print ('Универсумы:')
+    #print (str(l1)[1:-1]) or print (str(l1).strip('[]'))
+    print ('  '.join(map(str, l1))+'\n'+'  '.join(map(str, l2)),'\n')
+    print ('  '.join(map(str, l3))+'\n'+'  '.join(map(str, l4)),'\n')
     #---------------------------
-    print ('\nНоситель:')
+    print ('Носитель:')
     for i in range(len(l1)):
         if l1[i]>0:
             print (l2[i],' ',end='')
@@ -57,7 +62,7 @@ def printPropertyFuzzySet(l0,l1,l2,l3,l4):
     for i in range(len(l1)):
         if l1[i]==1:
             tmp+=1
-    if tmp==0: print ('Да')
+    if tmp==1: print ('Да')
     else: print ('Нет')
     #---------------------------
     print ('Y унимодально: ',end='')
@@ -65,12 +70,29 @@ def printPropertyFuzzySet(l0,l1,l2,l3,l4):
     for i in range(len(l1)):
         if l1[i]==1:
             tmp+=1
-    if tmp==0: print ('Да')
+    if tmp==1: print ('Да')
     else: print ('Нет')
     #---------------------------
-    print ('Высота: ',end='')
-    
-  
+    print ('Высота X: ',max(l1),'\nВысота Y: ',max(l3),'\n',end='')
+    #---------------------------
+    if max(l1)==1.0: print ('Нечеткое множество X - нормально')
+    else: print ('Нечеткое множество X - субнормально')
+    if max(l3)==1.0: print ('Нечеткое множество Y - нормально')
+    else: print ('Нечеткое множество Y - субнормально')   
+    #---------------------------
+    print ('Ядро X: ',end='')
+    for i in range(len(l1)): 
+        if l1[i]==1: print (l2[i],' ',end='')     
+    print ('\nЯдро Y: ',end='')
+    for i in range(len(l3)): 
+        if l3[i]==1: print (l4[i],' ',end='')   
+    #---------------------------
+    print ('\nГраница X: ',end='') 
+    for i in range(len(l1)):
+        if l1[i]>0 and l1[i]<1: print (l2[i],' ',end='')  
+    print ('\nГраница Y: ',end='') 
+    for i in range(len(l3)):
+        if l3[i]>0 and l3[i]<1: print (l4[i],' ',end='') 
 
 '''
 
@@ -118,7 +140,9 @@ print (listData1)
 print (listData2)
 print (listData3)
 print (listData4)
-printPropertyFuzzySet(listData0,listData1,listData2,listData3,listData4)
+printPropertyFuzzySet(listData1,listData2,listData3,listData4)
+subnormalToNormal(listData1,listData2,listData3,listData4)
+ploting(listData0)
 
 
 
